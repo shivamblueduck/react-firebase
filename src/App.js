e557be9abb10bf5307f46df1ab03b265d23fb638
SAM24 Shivam <sam24_shivam@Samviddhis-MacBook-Airshivam.local>
2023-09-10 22:15:29 +0530

    project push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
+import { auth } from "./firebase";
+
 
 function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
+
   return (
     <div className="App">
      <Router>
       <Routes>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home name={userName} />} />
 
 
         
